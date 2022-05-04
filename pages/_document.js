61b05a9f1c31bfa84cedbaad90b7df32484c0889
mnkhod <mnkhod.dev@gmail.com>
2022-05-04 16:26:21 +0800

    Added New Logo & Updated Meta & FavIcon

--- pages/_document.js
+++ pages/_document.js
@@ -9,26 +9,26 @@ export default function Document() {
         <meta name='application-name' content='Diverse EthCalculator' />
         <meta name='description' content='Diverse Eth Calculator' />
 
-        {/* <meta name='apple-mobile-web-app-capable' content='yes' /> */}
-        {/* <meta name='apple-mobile-web-app-status-bar-style' content='default' /> */}
-        {/* <meta name='apple-mobile-web-app-title' content='Diverse EthCalculator' /> */}
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='Diverse EthCalculator' />
 
-        {/* <meta name='format-detection' content='telephone=no' /> */}
-        {/* <meta name='mobile-web-app-capable' content='yes' /> */}
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
         {/* <meta name='msapplication-config' content='/icons/browserconfig.xml' /> */}
         {/* <meta name='msapplication-TileColor' content='#2B5797' /> */}
         {/* <meta name='msapplication-tap-highlight' content='no' /> */}
-        {/* <meta name='theme-color' content='#000000' /> */}
+        <meta name='theme-color' content='#000000' />
 
-        {/* <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' /> */}
-        {/* <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' /> */}
-        {/* <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' /> */}
-        {/* <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' /> */}
+        <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
+        <link rel='apple-touch-icon' sizes='152x152' href='/ios/152.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/ios/180.png' />
+        <link rel='apple-touch-icon' sizes='167x167' href='/ios/167.png' />
 
-        {/* <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' /> */}
-        {/* <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' /> */}
-        {/* <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' /> */}
-        {/* <link rel='shortcut icon' href='/favicon.ico' /> */}
+        <link rel='icon' type='image/png' sizes='32x32' href='/ios/32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/ios/32.png' />
+        <link rel='mask-icon' type='image/png' href='/ios/32.png' />
+        <link rel='shortcut icon' href='/ios/32.png' />
 
         <link rel='manifest' href='/manifest.json' />
 
@@ -38,6 +38,7 @@ export default function Document() {
         <meta name='twitter:description' content='Diverse Eth Calculator' />
         <meta name='twitter:image' content='https://ethcalculator.dsolutions.mn/android/android-launchericon-192-192.png' />
         <meta name='twitter:creator' content='@0xMnkhod' />
+
         <meta property='og:type' content='website' />
         <meta property='og:title' content='Diverse EthCalc' />
         <meta property='og:description' content='Diverse Eth Calculator' />
